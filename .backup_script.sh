79f751bf5fff6e95aad981e77e6c94ad83cefb25
Pedro Caetano <pcaetano1999@gmail.com>
2022-05-06 15:57:46 +0100

    Added comments

--- .backup_script.sh
+++ .backup_script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# start ssh and add identity, in ~/.bashrc file
+#
+# eval $(ssh-agent)
+# ssh-add ~/.ssh/id_rsa
+#
+# crontab -e, 0 * * * * /home/pi/klipper_config/.backup_script.sh
+
 CURRENTDATE=`date +"%d-%m-%Y %T"`
 
 git add *
